c16af03a25c7f1a16702a79b739edf697354a93e
ByteDance <daquanzhou@bytedance.com>
2022-11-20 22:41:47 +0800

    modify style

--- js/main.js
+++ js/main.js
@@ -1,147 +1,21 @@
 jQuery(document).ready(function($) {
 
-
-  const data_list = [
-    {
-      'link':'./img/1.gif',
-      'name': '',
-      'header': '',
-      'text':'A confused grizzly bear in calculus class.'
-    },
-    {
-      'link':'./img/2.gif',
-      'name': '',
-      'header': '',
-      'text':'A person riding a horse in the sunrise.'
-    },
-    {
-      'link':'./img/3.gif',
-      'name': '',
-      'header': '',
-      'text':'Coffee pouring into a cup.'
-    },
-    {
-      'link':'./img/4.gif',
-      'name': '',
-      'header': '',
-      'text':'An astronaut is riding a horse in the space in a photorealistic style'
-    },
-    {
-      'link':'./img/5.gif',
-      'name': '',
-      'header': '',
-      'text':'Robot dancing in times square'
-    },
-    {
-      'link':'./img/6.gif',
-      'name': '',
-      'header': '',
-      'text':'Drone flythrough of a tropical jungle covered in snow.'
-    },
-    {
-      'link':'./img/7.gif',
-      'name': '',
-      'header': '',
-      'text':'A beautiful sunrise on mars, Curiosity rover. High definition, timelapse, dramatic colors.'
-    },
-    {
-      'link':'./img/8.gif',
-      'name': '',
-      'header': '',
-      'text':'Wooden figurine walking on a treadmill made out of exercise mat'
-    },
-    {
-      'link':'./img/9.gif',
-      'name': '',
-      'header': '',
-      'text':'Sprouts in the shape of text ’Imagen’ coming out of a fairytale book.'
-    },
-    {
-      'link':'./img/10.gif',
-      'name': '',
-      'header': '',
-      'text':'Hyper-realistic spaceship landing on mars.'
-    },
-    {
-      'link':'./img/11.gif',
-      'name': '',
-      'header': '',
-      'text': 'A tiger with fur made out of electricity, digital art.'
-    },
-    {
-      'link':'./img/12.gif',
-      'name': '',
-      'header': 'Origami dancers in white paper, 3D render, ultra-detailed, on white background, studio shot, dancing modern dance.',
-      'text': ''
-    },
-    {
-      'link':'./img/13.gif',
-      'name': '',
-      'header': '',
-      'text': 'Campfire at night in a snowy forest with starry sky in the background.'
-    },
-    {
-      'link':'./img/14.gif',
-      'name': '',
-      'header': 'Drone flythrough of a tropical jungle covered in snow.',
-      'text': ''
-    },
-    {
-      'link':'./img/15.gif',
-      'name': '',
-      'header': '',
-      'text': 'A blue unicorn flying over a mystical land.'
-    },
-    {
-      'link':'./img/16.gif',
-      'name': '',
-      'header': '',
-      'text': 'Drone flythrough of a pixel art of futuristic city.'
-    },
-    {
-      'link':'./img/17.gif',
-      'name': '',
-      'header': '',
-      'text': 'Drone flythrough of a tropical jungle covered in snow. High Definition.'
-    },
-    {
-      'link':'./img/18.gif',
-      'name': '',
-      'header': '',
-      'text': 'A cat eating food out of a bowl, in style of Van Gogh.'
-    },
-    {
-      'link':'./img/19.gif',
-      'name': '',
-      'header': '',
-      'text': 'A 3D model of an elephant origami. Studio lighting.'
-    },
-    {
-      'link':'./img/20.gif',
-      'name': '',
-      'header': '',
-      'text': 'iPouring latte art into a silver cup with a golden spoon next to it.'
-    },
-  ]
-
-
-  for (data of data_list) {
+  //for (data of data_list) {
+  for(i = 0; i < 20; i++) {
+    data = data_list[i];
   if (document.getElementById('image-container')!= null){
     document.getElementById('image-container').innerHTML += `
-  
-
-    <a href=${data.link} data-lightbox="image-1"><div class="thumb">
-        <div class="hover-effect">
+    <a href=${data.link} data-lightbox="image-1">
+    <div class="thumb">
+        <div class="hover-effect" name=${data.name} hidden="true">
             <div class="hover-content">
-                 <h1 id="onDesktop">${data.name} <em>${data.header}</em></h1>
-                 <p id="onDesktop">${data.text}</p>
+                 <p>${data.text}</p>
             </div>
         </div>
-        <div class="image">
-        <img src=${data.link} alt="imgae not in path">
-    </div>
+        <div class="image" >
+<img src=${data.link} alt="image not in path" id=${data.name} onmouseover="mouseover(this)" onmouseout="mouseout(this)"/>
+        </div>
     </div></a>
-
 `;
 
   }
@@ -149,6 +23,26 @@ jQuery(document).ready(function($) {
 }
 
 	'use strict';
+
+        $('#loadmore').click(function(){
+            document.getElementById('loadmorediv').hidden = true;
+            for(i = 20; i < data_list.length; i++) {
+                data=data_list[i];
+                document.getElementById('image-container').innerHTML += `
+  		  <a href=${data.link} data-lightbox="image-1">
+    		  <div class="thumb">
+                    <div class="hover-effect" name=${data.name} hidden="true">
+                      <div class="hover-content">
+                        <p>${data.text}</p>
+                      </div>
+                    </div>
+                    <div class="image" >
+                      <img src=${data.link} alt="image not in path" id=${data.name} onmouseover="mouseover(this)" onmouseout="mouseout(this)"/>
+                    </div>
+             </div></a>
+`;
+            }
+        });
 
         $(function() {
   
